100d63f7146b47918fbf0202236469ce9a409806
Shubhamitra Das <38852595+shubhamitradas@users.noreply.github.com>
2018-07-13 11:07:03 +0530

    Update model.py

--- toxic/models/model.py
+++ toxic/models/model.py
@@ -14,7 +14,7 @@ from allennlp.nn.regularizers import RegularizerApplicator
 import allennlp.nn.util as util
 from allennlp.training.metrics import BooleanAccuracy
 
-from toxic.training.metrics.multilabel_f1 import MultiLabelF1Measure
+from training.metrics.multilabel_f1 import MultiLabelF1Measure
 
 @Model.register("toxic")
 class ToxicModel(Model):
